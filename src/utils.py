ff7d644f45f6626cdd399ae01bec01d17bc02dd1
Akshat Chaurasia <akshatchaurasia212005@gmail.com>
2025-12-14 21:15:37 +0530

    Post Params

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
+from src.logger import logging
 from src.exception import CustomException
 
 def save_obj(file_path,obj):
@@ -21,13 +23,24 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train, X_test, y_train, y_test, models):
+def evaluate_model(X_train, X_test, y_train, y_test, models,param):
     try:
         report = {}
         
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
+            if model_name == "CatBoosting Regressor":
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_test)
+                score = r2_score(y_test, y_pred)
+                report[model_name] = score
+                continue
+            params_ = param.get(model_name, {})
+            gs = GridSearchCV(model, params_, cv=3)
+            gs.fit(X_train, y_train)
+            
+            best_model = gs.best_estimator_
+            y_pred = best_model.predict(X_test)
+
             
             score = r2_score(y_test, y_pred)
             report[model_name] = score
